7374156c7a86737f2ae203ad53549f684d3a50de
Omar Stewey <OmarStewey@Outlook.com>
2025-04-07 17:45:02 -0400

    init

---
+++ ajatt-links.js
@@ -0,0 +1,63 @@
+/*
+Called when the item has been created, or when creation failed due to an error.
+Log failure.
+*/
+function onCreated() {
+    if (browser.runtime.lastError) {
+      console.error(`Error: ${browser.runtime.lastError}`);
+    }
+  }
+
+  /*
+  Create all the context menu items.
+  */
+
+  
+  browser.menus.create({
+    id: "kakimashou",
+    title: "書きましょう",
+    contexts: ["selection"]
+  }, onCreated);
+  
+
+  browser.menus.create({
+    id: "rtk",
+    title: "RTK",
+    contexts: ["selection"]
+  }, onCreated);
+  
+
+  browser.menus.create({
+    id: "kanji-alive",
+    title: "Kanji Alive",
+    contexts: ["selection"]
+  }, onCreated);
+  
+  /*
+  On Right Click 
+  */
+
+  browser.menus.onClicked.addListener((info, tab) => {
+    
+    var selectedText = info.selectionText;
+
+    if(!!selectedText == false){
+      return;
+    }
+
+    var firstChar = selectedText.charAt(0);
+
+    switch (info.menuItemId) {
+      case "kakimashou":
+        browser.tabs.create({url: "https://www.kakimashou.com/dictionary/character/" + firstChar});
+        break;
+        case "rtk":
+        browser.tabs.create({url: "https://kanji.koohii.com/study/kanji/" + firstChar});
+        break;
+      case "kanji-alive":
+        browser.tabs.create({url: "https://app.kanjialive.com/" + firstChar});
+        break;
+      default:
+        break;
+    }
+  });
---
+++ readme.md
@@ -0,0 +1,5 @@
+# AJATT Links
+Links for helping with AJATT. Mainly Kanji.
+
+## Usage
+Select Text, then Right Click.
