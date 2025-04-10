0fe9728b670ce8cbf1c37c306a176fe0ffc87388
Omar Stewey <OmarStewey@Outlook.com>
2025-04-10 11:14:15 -0400

    Add Jisho Search

--- ajatt-links.js
+++ ajatt-links.js
@@ -4,60 +4,69 @@ Log failure.
 */
 function onCreated() {
     if (browser.runtime.lastError) {
-      console.error(`Error: ${browser.runtime.lastError}`);
+        console.error(`Error: ${browser.runtime.lastError}`);
     }
-  }
+}
 
-  /*
-  Create all the context menu items.
-  */
+/*
+Create all the context menu items.
+*/
 
-  
-  browser.menus.create({
+
+browser.menus.create({
     id: "kakimashou",
     title: "書きましょう",
     contexts: ["selection"]
-  }, onCreated);
-  
+}, onCreated);
 
-  browser.menus.create({
+
+browser.menus.create({
     id: "rtk",
     title: "RTK",
     contexts: ["selection"]
-  }, onCreated);
-  
+}, onCreated);
 
-  browser.menus.create({
+
+browser.menus.create({
     id: "kanji-alive",
     title: "Kanji Alive",
     contexts: ["selection"]
-  }, onCreated);
-  
-  /*
-  On Right Click 
-  */
+}, onCreated);
 
-  browser.menus.onClicked.addListener((info, tab) => {
-    
+browser.menus.create({
+    id: "jisho",
+    title: "Jisho",
+    contexts: ["selection"]
+}, onCreated);
+
+/*
+On Right Click 
+*/
+
+browser.menus.onClicked.addListener((info, tab) => {
+
     var selectedText = info.selectionText;
 
-    if(!!selectedText == false){
-      return;
+    if (!!selectedText == false) {
+        return;
     }
 
     var firstChar = selectedText.charAt(0);
 
     switch (info.menuItemId) {
-      case "kakimashou":
-        browser.tabs.create({url: "https://www.kakimashou.com/dictionary/character/" + firstChar});
-        break;
+        case "kakimashou":
+            browser.tabs.create({ url: "https://www.kakimashou.com/dictionary/character/" + firstChar });
+            break;
         case "rtk":
-        browser.tabs.create({url: "https://kanji.koohii.com/study/kanji/" + firstChar});
-        break;
-      case "kanji-alive":
-        browser.tabs.create({url: "https://app.kanjialive.com/" + firstChar});
-        break;
-      default:
-        break;
+            browser.tabs.create({ url: "https://kanji.koohii.com/study/kanji/" + firstChar });
+            break;
+        case "kanji-alive":
+            browser.tabs.create({ url: "https://app.kanjialive.com/" + firstChar });
+            break;
+        case "jisho":
+            browser.tabs.create({ url: "https://jisho.org/search/" + selectedText });
+            break;
+        default:
+            break;
     }
-  });
+});
